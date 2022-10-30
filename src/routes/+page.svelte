cfb58f6c42d4834830b90859a6054feda1bc8abb
edewindt <edewindt27@outlook.com>
2022-10-30 03:27:55 -0500

    Seperated cards from pairs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,21 @@
 	import metal from '$lib/metal.jpg';
 	import psychic from '$lib/psychic.jpg';
 	import water from '$lib/water.jpg';
+	import back from '$lib/back.jpg';
 	let err = 0;
 	let board;
-	let cardList = [darkness, double, fairy, fighting, fire, grass, lightning, metal, psychic, water];
+	let cardList = [
+		{ n: darkness, b: false },
+		{ n: double, b: false },
+		{ n: fairy, b: false },
+		{ n: fighting, b: false },
+		{ n: fire, b: false },
+		{ n: grass, b: false },
+		{ n: lightning, b: false },
+		{ n: metal, b: false },
+		{ n: psychic, b: false },
+		{ n: water, b: false }
+	];
 	let cardSet;
 	let boardc = [];
 	onMount(() => {
@@ -20,7 +32,19 @@
 	});
 
 	const shuffleCards = () => {
-		cardSet = cardList.concat(cardList); // Doubling cards
+		let cardSet1 = [];
+		let cardSet2 = [];
+		cardList.forEach((c) => {
+			c = { ...c, list: 1 };
+			cardSet1 = [...cardSet1, c];
+		});
+		cardList.forEach((c) => {
+			c = { ...c, list: 2 };
+			cardSet2 = [...cardSet2, c];
+		});
+		console.log(cardSet1);
+		console.log(cardSet2);
+		cardSet = cardSet1.concat(cardSet2); // Doubling cards
 		console.log(cardSet);
 		//Shuffle cards
 		for (let i = 0; i < cardSet.length; i++) {
@@ -38,7 +62,17 @@
 <div id="board" bind:this={board}>
 	{#if cardSet}
 		{#each cardSet as c}
-			<img class="card" src={c} alt="" srcset="" />
+			{#if c.b}<button
+					on:click={() => {
+						c.b = !c.b;
+					}}><img class="card" src={c.n} alt="" srcset="" /></button
+				>{:else}<button
+					on:click={() => {
+						c.b = !c.b;
+					}}
+				>
+					<img class="card" src={back} alt="" />
+				</button>{/if}
 		{/each}
 	{/if}
 </div>
